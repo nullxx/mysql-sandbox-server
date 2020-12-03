fea54bc685f957bf6e76847d70f29b927708251e
Jon Lara <jon.laratrigo@gmail.com>
2020-12-03 19:12:33 +0100

    Catch middleware resumeDbFromSession

--- nodejs-microservice/src/api/middlewares/session.js
+++ nodejs-microservice/src/api/middlewares/session.js
@@ -3,16 +3,21 @@ const libMysql = require('../../lib/mysql');
 
 const resumeDbFromSession = async (req, _res, next) => {
   const { connections } = req.app.locals;
-  for (let i = 0; i < connections.length; i += 1) {
-    const conn = connections[i];
-    if (conn.session === req.session.id) {
-      // eslint-disable-next-line no-await-in-loop
-      req.mysql = await getConnection(conn.mysql);
-      // eslint-disable-next-line no-await-in-loop
-      await libMysql.changeUser(req.mysql, conn.dbName);
-      // disable eslint because it will only be executed once
-      return next();
+  try {
+    for (let i = 0; i < connections.length; i += 1) {
+      const conn = connections[i];
+      if (conn.session === req.session.id) {
+        // eslint-disable-next-line no-await-in-loop
+        req.mysql = await getConnection(conn.mysql);
+        // eslint-disable-next-line no-await-in-loop
+        await libMysql.changeUser(req.mysql, conn.dbName);
+        // disable eslint because it will only be executed once
+
+        return next();
+      }
     }
+  } catch (error) {
+    next(error);
   }
   return next(new Error('Please create a database'));
 };
@@ -29,14 +34,14 @@ const resumeDbFromSessionSoft = (req, _res, next) => {
   return next();
 };
 const closeResumedDbSessionError = (err, req, _res, next) => {
-    req.mysql.release();
-    next(err);
+  req.mysql.release();
+  next(err);
 }
 const closeResumedDbSession = (req, _res, next) => {
-// this will be executed only when error ocurred
+  // this will be executed only when error ocurred
   try {
     if (req.mysql) { // an error could be thrown before set req.mysql
-        req.mysql.release();
+      req.mysql.release();
     }
   } catch (error) {
     next(error);
